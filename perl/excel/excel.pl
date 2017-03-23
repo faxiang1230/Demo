142e369d8e6c1d06a85c32b6d2c6a4ae04a0d75f
Jianxing Wang <wangjianxing5210@163.com>
2017-03-23 22:53:49 +0800

    perl:awk filter and write into xlsx
    Signed-off-by: Jianxing Wang <wangjianxing5210@163.com>

--- perl/excel/excel.pl
+++ perl/excel/excel.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/perl -w
 use Spreadsheet::Read;  
 use Excel::Writer::XLSX;  
-  
 my $workbook = Excel::Writer::XLSX->new('b.xlsx');  
 my $worksheet = $workbook->add_worksheet();  
   
@@ -11,7 +10,13 @@ my $format1 = $workbook->add_format(
         bg_color => 'red', 
         color => 'green'  
     );  
-  
+open my $date,"sar -P ALL|awk '/all/{print \$3,\$4}'|";
+while(1) {
+	my $line = readline $date;
+	last unless defined $line;
+	my @ncloum=split(/\s+/,$line);
+	print "test".@ncloum[1]."\n";
+}
 $worksheet->write( 1,1, 'try fromat',$format1 );  
   
 $workbook->close();
