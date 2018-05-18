71d042e767fdd794f683323e2163608d6eb3afab
Jianxing.Wang <wangjianxing5210@163.com>
2018-05-18 13:43:04 +0800

    update

--- kernel/wait-queue.c
+++ kernel/wait-queue.c
@@ -8,19 +8,26 @@
 #include <linux/uaccess.h>
 #include <linux/sched/signal.h>
 #include <linux/platform_device.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <linux/sched.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/poll.h>
+#include <linux/miscdevice.h>
+#include <linux/of_device.h>
 
 #define FIFO_SIZE 1024
 
-struct myfifo {
+typedef struct {
 	struct mutex myfifo_lock;
 	int current_count;
 	char mem[FIFO_SIZE];
 	wait_queue_head_t r_wait;
 	wait_queue_head_t w_wait;
 	struct miscdevice miscdev;
-};
-typedef struct myfifo myfifo_t;
-
+}myfifo_t;
+static myfifo_t *g_fifo;
 static int myfifo_open(struct inode *ip, struct file *fp)
 {
 	return 0;
@@ -28,10 +35,8 @@ static int myfifo_open(struct inode *ip, struct file *fp)
 static ssize_t myfifo_read(struct file *fp, char __user *buf,
 		size_t nbytes, loff_t *ppos) {
 	int ret = 0;
-	myfifo_t *g_fifo = container_of(fp->private_data, myfifo_t, miscdev);
 	DECLARE_WAITQUEUE(wait, current);
 	add_wait_queue(&g_fifo->r_wait, &wait);
-
 
 	while(g_fifo->current_count == 0) {
         if (fp->f_flags & O_NONBLOCK) {
@@ -44,13 +49,15 @@ static ssize_t myfifo_read(struct file *fp, char __user *buf,
 			ret = -ERESTARTSYS;
 			goto out;
 		}	
+		pr_info("my_fifo no data,sleep\n");
 		schedule();
 	}
-read:
+	pr_info("my_fifo have %d data\n", g_fifo->current_count);
 	if(nbytes > g_fifo->current_count)
 		nbytes = g_fifo->current_count;
 
 	if(!copy_to_user(buf, g_fifo->mem, nbytes)) {
+		pr_info("my_fifo read %d data\n", nbytes);
 		g_fifo->current_count -= nbytes;
 		memcpy(g_fifo->mem, g_fifo->mem + nbytes, g_fifo->current_count);
 		ret = nbytes;
@@ -66,12 +73,12 @@ out:
 static ssize_t myfifo_write(struct file *fp, const char __user *buffer,
 				  size_t nbytes, loff_t *ppos) {
 	int ret = 0;
-	myfifo_t *g_fifo = container_of(fp->private_data, myfifo_t, miscdev);
 
 	DECLARE_WAITQUEUE(wait, current);
 	add_wait_queue(&g_fifo->w_wait, &wait);
 retry:
-	if(nbytes + g_fifo->current_count > FIFO_SIZE) {
+	if((nbytes + g_fifo->current_count) > FIFO_SIZE) {
+		pr_info("my_fifo write %d data and current:%d\n", nbytes, g_fifo->current_count);
 		copy_from_user(g_fifo->mem + g_fifo->current_count, buffer, FIFO_SIZE - g_fifo->current_count);
 		nbytes -= (FIFO_SIZE - g_fifo->current_count);
 		g_fifo->current_count = FIFO_SIZE;
@@ -84,7 +91,10 @@ retry:
 			goto retry;
 		}
 	} else {
-		ret += copy_from_user(g_fifo->mem + g_fifo->current_count, buffer, nbytes);	
+		pr_info("my_fifo write %d data\n", nbytes);
+		copy_from_user(g_fifo->mem + g_fifo->current_count, buffer, nbytes);	
+		ret = nbytes;
+		g_fifo->current_count += nbytes;
 		wake_up_interruptible(&g_fifo->r_wait);
 	}
 	remove_wait_queue(&g_fifo->w_wait, &wait);
@@ -96,8 +106,8 @@ static struct file_operations myfifo_ops = {
 	.write = myfifo_write,
 	.open = myfifo_open,
 };
-static int myfifo_init(struct platform_device *pdev) {
-	myfifo_t *g_fifo = devm_kzalloc(&pdev->dev, FIFO_SIZE, GFP_KERNEL);
+static int __init myfifo_init(void) {
+	g_fifo = kzalloc(sizeof(myfifo_t), GFP_KERNEL);
 	mutex_init(&g_fifo->myfifo_lock);
 	g_fifo->current_count = 0;
 	init_waitqueue_head(&g_fifo->r_wait);
@@ -107,27 +117,17 @@ static int myfifo_init(struct platform_device *pdev) {
 	g_fifo->miscdev.minor = MISC_DYNAMIC_MINOR;
 	g_fifo->miscdev.fops = &myfifo_ops;
 	misc_register(&g_fifo->miscdev);
-	platform_set_drvdata(pdev, g_fifo);
 
-	printk(KERN_DEBUG "my work init\n");
+	printk(KERN_INFO "my fifo init\n");
 	return 0;
 }
 
-static int myfifo_exit(struct platform_device *pdev) {
-	myfifo_t *g_fifo = platform_get_drvdata(pdev);
+static void __exit myfifo_exit(void) {
 	misc_deregister(&g_fifo->miscdev);
-	return 0;
 }
-static struct platform_driver myfifo_driver = { 
-    .driver = { 
-        .name = "myfifo",
-        .owner = THIS_MODULE,
-    },  
-    .probe = myfifo_init,
-    .remove = myfifo_exit,
-};
 
-module_platform_driver(myfifo_driver);
+module_init(myfifo_init);
+module_exit(myfifo_exit);
 
 MODULE_AUTHOR("Jianxing.Wang");
 MODULE_DESCRIPTION("fifo demo");
