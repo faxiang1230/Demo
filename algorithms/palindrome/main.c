72f33ec01f345dfa03f3bf2996eadd0cdeb625e4
Jianxing.Wang <wangjianxing5210@163.com>
2019-02-19 14:44:04 +0800

    quick and slow pointer to check palindrome string

--- algorithms/palindrome/main.c
+++ algorithms/palindrome/main.c
@@ -2,39 +2,116 @@
     > File Name: main.c
     > Author: wangjx
  ************************************************************************/
-#include "slist.h"
-int palindrome(SListEntry *entry)
+#include <stdio.h>
+#include <stdlib.h>
+typedef char SListValue;
+typedef struct _SListEntry {
+	SListValue data;
+	struct _SListEntry *next;
+}SListEntry;
+
+SListEntry *slist_append(SListEntry **list, SListValue data)
 {
-	int odd = 0;
-	SListEntry *fast = entry, *slow = entry, *last = NULL;
-	SListEntry *head = NULL;
+	SListEntry *rover;
+	SListEntry *newentry;
 
-	while(fast && fast->next) {
-		if (fast->next->next) {
-			fast = fast->next->next;
-		} else {
-			odd = 1;
-			fast = fast->next;
-			break;
-		}
+	/* Create new list entry */
 
-		slow->next = last;
-		last = slow;
-		slow = slow->next;
+	newentry = malloc(sizeof(SListEntry));
+
+	if (newentry == NULL) {
+		return NULL;
 	}
-	if (odd) {
-		head1 = slow;
-		head2 = slow->next;
+
+	newentry->data = data;
+	newentry->next = NULL;
+
+	/* Hooking into the list is different if the list is empty */
+
+	if (*list == NULL) {
+
+		/* Create the start of the list */
+
+		*list = newentry;
+
 	} else {
-		head2 = 
+
+		/* Find the end of list */
+
+		for (rover=*list; rover->next != NULL; rover = rover->next);
+
+		/* Add to the end of list */
+
+		rover->next = newentry;
+	}
+
+	return newentry;
+}
+void slist_free(SListEntry *list)
+{
+	SListEntry *entry;
+
+	/* Iterate over each entry, freeing each list entry, until the
+	 * end is reached */
+
+	entry = list;
+
+	while (entry != NULL) {
+		SListEntry *next;
+
+		next = entry->next;
+
+		free(entry);
+
+		entry = next;
 	}
 }
+
+int palindrome(SListEntry *entry)
+{
+	int odd = 0, ret = 0;
+	SListEntry *fast = entry, *slow = entry, *last = NULL, *next = NULL;
+	SListEntry *head1 = NULL, *head2 = NULL;
+
+	while(fast && fast->next) {
+		fast = fast->next->next;
+
+		next = slow->next;
+		slow->next = last;
+		last = slow;
+		slow = next;
+	}
+
+	if (fast) {
+		head1 = last;
+		head2 = slow->next;
+		//slow->next = last;
+	} else {
+		head2 = last; 
+		head1 = slow;
+	}
+
+	while(head1 && head2) {
+		//printf("%c:%c\n", head1->data, head2->data);
+		if (head1->data != head2->data)
+			return 0;
+		head1 = head1->next;
+		head2 = head2->next;
+	}
+	return 1;
+}
+
 void str2list(char *str)
 {
+	int ret = 0;
 	SListEntry *list = NULL;
 	char *ptr = str;
-	while(*str++)
-		slist_append(&list, str);
+	while(*str) {
+		slist_append(&list, *str);
+		//printf("%c ", *str);
+		str++;
+	}
+	//printf("\n");
 	
 	ret = palindrome(list);
 
