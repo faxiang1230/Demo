8eb1bc011b478a123668df47ac54c40a09d923b8
Jianxing.Wang <wangjianxing5210@163.com>
2019-11-22 01:04:27 +0800

    rbtree insert ok

--- algorithms/tree/rb-tree.c
+++ algorithms/tree/rb-tree.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
-enum color_t{RED, BLACK};
+typedef enum {RED, BLACK}color_t;
 typedef struct rb_node{
 	struct rb_node *parent, *left, *right;
 	int val;
-	enum color_t color;
+	color_t color;
 }node_t;
 
 node_t *root = NULL;
 
 node_t *new_node()
 {
-	node_t *node = calloc(1, *node);
-	node.color = RED;
+	node_t *node = calloc(1, sizeof(node_t));
+	node->color = RED;
 	return node;
 }
 
@@ -32,8 +33,56 @@ node_t *get_uncle(node_t *node)
 	node_t *grand = parent->parent;
 	if (grand->left == parent)
 		return grand->right;
-	else if (gradn->right == parent)
+	else if (grand->right == parent)
 		return grand->left;
+}
+void left_rotate(node_t *node)
+{
+    node_t *right = node->right;
+
+    if (!right)
+        return;
+    //assert(node->parent);
+    if (node->parent) {
+        if (node->parent->left == node) {
+            node->parent->left = right;
+        } else if (node->parent->right == node) {
+            node->parent->right = right;
+        }
+        right->parent = node->parent;
+    } else {
+        root = right;
+        right->parent = NULL;
+    }
+
+    node->right = right->left;
+    if (right->left) right->left->parent = node;
+    node->parent = right;
+    right->left = node;
+}
+void right_rotate(node_t *node)
+{
+    node_t *left = node->left;
+
+    //assert(node->parent);
+    if (!left)
+        return;
+    if (node->parent) {
+        if (node->parent->left == node) {
+            node->parent->left = left;
+        } else if (node->parent->right == node) {
+            node->parent->right = left;
+        }
+        left->parent = node->parent;
+    } else {
+        root = left;
+        left->parent = NULL;
+    }
+
+    node->left = left->right;
+    if (left->right) left->right->parent = node;
+    node->parent = left;
+    left->right = node;
 }
 
 void insert_case1(node_t *node)
@@ -44,24 +93,61 @@ void insert_case1(node_t *node)
 		insert_case2(node);
 	}
 }
+void insert_case2(node_t *node)
+{
+    if (node->parent->color != BLACK) {
+        insert_case3(node);
+    }
+}
 void insert_case3(node_t *node)
 {
-	node_t *uncle = NULL, *grand = NULL;	
+	node_t *uncle = NULL, *grand = NULL, *parent = NULL;	
 
-	grand = node->parent->parent;
+    parent = node->parent;
+	grand = parent->parent;
 	uncle = get_uncle(node);
 
-	if (uncle != NULL && uncle->color = RED) {
+	if (uncle != NULL && uncle->color == RED) {
 		uncle->color = BLACK;
 		parent->color = BLACK;
 		grand->color = RED;
-	}
+        insert_case1(grand);
+	} else {
+        insert_case4(node);
+    }
 }
-void insert_node(node_t *p, int val)
+void insert_case4(node_t *node) {
+    node_t *new_node = node;
+    if (node->parent->right == node) {
+        if (node->parent->parent && node->parent->parent->left == node->parent) {
+            new_node = node->parent;
+            left_rotate(node->parent);
+        }
+    } else if(node->parent->left == node) {
+        if (node->parent->parent && node->parent->parent->right == node->parent) {
+            new_node = node->parent;
+            right_rotate(node->parent);
+        }
+    }
+    insert_case5(new_node);
+}
+void insert_case5(node_t *node)
+{
+    node_t *parent = node->parent;
+    node_t *grand = parent->parent;
+
+    if (parent->left == node)
+        right_rotate(grand);
+    else
+        left_rotate(grand);
+    parent->color = BLACK;
+    grand->color = RED;
+}
+void insert_val2(node_t *p, int val)
 {
 	node_t *rover = p;
 	node_t *parent = NULL;
-	rb_node_t *node = new_node();
+    node_t *node = new_node();
 	node->val = val;
 
 	while(rover) {
@@ -76,7 +162,7 @@ void insert_node(node_t *p, int val)
 	if (node->val > parent->val)
 		parent->right = node;
 	else if (node->val < parent->val)
-		parent->left = left;
+		parent->left = node;
 
 	node->parent = parent;
 
@@ -89,14 +175,52 @@ void insert_node(node_t *p, int val)
 void insert_val(int val)
 {
 	if (!root) {
-		rb_node_t *node = new_node();
+		node_t *node = new_node();
 		root = node;
 		node->val = val;
 		node->color = BLACK;
 		return;
 	}
 
-	insert_node(root, val);
+	insert_val2(root, val);
 }
 
-
+void display_subtree(node_t *node)
+{
+	if (!node)
+		return;
+	display_subtree(node->left);
+	printf("%d<-%d(%s) ", node->parent?node->parent->val:-1, node->val, node->color == RED?"R":"B");
+	display_subtree(node->right);
+}
+void display_tree()
+{
+    if (root) {
+	    printf("[root %d(%s)] ", root->val, root->color == RED?"R":"B");
+        display_subtree(root);
+    }
+    printf("\n");
+}
+void main()
+{
+    insert_val(3);
+    display_tree();
+    insert_val(1);
+    display_tree();
+    insert_val(2);
+    display_tree();
+    insert_val(4);
+    display_tree();
+    insert_val(5);
+    display_tree();
+    insert_val(6);
+    display_tree();
+    insert_val(7);
+    display_tree();
+    insert_val(8);
+    display_tree();
+    insert_val(9);
+    display_tree();
+    insert_val(10);
+    display_tree();
+}
