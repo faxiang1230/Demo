8e080f325858a4d0ca3db52e2aceb2db0fbeabc1
Jianxing.Wang <jxwang@zshield.net>
2019-11-26 20:43:11 +0800

    zero copy draft4

--- kernel/zero-copy/core.c
+++ kernel/zero-copy/core.c
@@ -11,17 +11,50 @@
 #include <linux/mm.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
+#include <linux/rbtree.h>
 
 struct log_queue {
 	int nr_pages;
 	volatile int head, head_next, tail, tail_next;
 	struct page **pages;
 	char *buffer;
-	struct address_space	mapping;
+	struct address_space mapping;
 	char buf[0];
+};
+
+#define NUM (100000000UL)
+#define OFFSET  16
+
+static void my_work(struct work_struct *work);
+DECLARE_DELAYED_WORK(mywork, my_work);
+static void my_work(struct work_struct *work) {
+	int flags_old, flags_new;
+	long long i = 0;
+	struct log_queue *queue = (struct log_queue *)(atomic_read(work->data));
+	int *val = (int *)(queue->buffer + OFFSET);
+	printk(KERN_ERR "begin %d\n", *val);
+
+	for (i = 0; i < NUM; i++) {
+		do {
+			flags_old = atomic_read(val);
+			flags_new = flags_old++;
+		} while (atomic_cmpxchg(val, flags_old, flags_new) != flags_old);
+	}
+	printk(KERN_ERR "end %d\n", *val);
+}
+
+static int log_queue_set_page_dirty(struct page *page)
+{
+	SetPageDirty(page);
+	return 0;
+}
+
+static const struct address_space_operations log_queue_aops = {
+	.set_page_dirty	= log_queue_set_page_dirty,
 };
 static int log_queue_release(struct inode *inode, struct file *file)
 {
+	printk(KERN_ERR "%s\n", __func__);
 	struct log_queue *queue = file->private_data;
 	kfree(queue);
 	return 0;
@@ -31,18 +64,24 @@ static int log_queue_open(struct inode *inode, struct file *file)
 {
 	struct log_queue *wqueue;
 
+	printk(KERN_ERR "%s\n", __func__);
 	wqueue = kzalloc(sizeof(*wqueue), GFP_KERNEL);
 	if (!wqueue)
 		return -ENOMEM;
+
+	wqueue->mapping.a_ops = &log_queue_aops;
+	wqueue->mapping.i_mmap = RB_ROOT;
 
 	file->private_data = wqueue;
 
 	return 0;
 }
+
 #define SET_PAGE	_IOW('t', 1, int)
 #define GET_PAGE	_IOR('t', 2, int)
 static long log_queue_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
+	printk(KERN_ERR "%s\n", __func__);
 	long ret = -1;
 	struct log_queue *queue = file->private_data;
 
@@ -65,16 +104,16 @@ static unsigned int log_queue_poll(struct file *file, struct poll_table_struct *
 }
 static void log_vm_open(struct vm_area_struct * area)
 {
-	printk("%s\n", __func__);
+	printk(KERN_ERR "%s\n", __func__);
 }
 static void log_vm_close(struct vm_area_struct * area)
 {
-	printk("%s\n", __func__);
+	printk(KERN_ERR "%s\n", __func__);
 }
 static int log_vm_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 {
-	printk("%s\n", __func__);
-	return VM_FAULT_NOPAGE;
+	printk(KERN_ERR "%s\n", __func__);
+	return VM_FAULT_SIGBUS;
 }
 static struct vm_operations_struct log_vm_ops = {
 	.open = log_vm_open,
@@ -86,39 +125,51 @@ static int log_queue_mmap (struct file *file, struct vm_area_struct *vma)
 	int i = 0;
 	char *buf = NULL;
 	struct log_queue *queue = file->private_data;
+
+	printk(KERN_ERR "%s %d pages start:%lx end:%lx\n", __func__, queue->nr_pages, vma->vm_start, vma->vm_end);
 	if ((vma->vm_end - vma->vm_start > queue->nr_pages * PAGE_SIZE)) {
 		return -EINVAL;
 	}
+
 	vma->vm_ops = &log_vm_ops;
 	vma->vm_flags |= VM_DONTEXPAND | VM_DONTDUMP;
 	vma->vm_private_data = file->private_data;
 
+	printk(KERN_ERR "%s 1\n", __func__);
 	queue->pages = kcalloc(queue->nr_pages, sizeof(struct page *), GFP_KERNEL);
 	if (!queue->pages)
 		goto err;
 
+	printk(KERN_ERR "%s 2\n", __func__);
 	for (i = 0; i < queue->nr_pages; i++) {
 		queue->pages[i] = alloc_page(GFP_KERNEL | __GFP_ZERO);
 		if (!queue->pages[i])
 			goto err_some_pages;
-		queue->pages[i]->mapping = &queue->mapping;
-		SetPageUptodate(queue->pages[i]);
+		//queue->pages[i]->mapping = &queue->mapping;
+		printk(KERN_ERR "%s %p insert page\n", __func__, vma->vm_start + (i * PAGE_SIZE));
+		vm_insert_page(vma, vma->vm_start + (i * PAGE_SIZE), queue->pages[i]);
 	}
+	printk(KERN_ERR "%s 3\n", __func__);
 
-	buf = vmap(queue->pages, queue->nr_pages, VM_MAP, PAGE_SHARED);
+	buf = vmap(queue->pages, queue->nr_pages, VM_MAP, PAGE_KERNEL);
 	if (!buf)
 		goto err_some_pages;
+	printk(KERN_ERR "%s 4\n", __func__);
 	queue->buffer = buf;
 	memcpy(buf, "hello", sizeof("hello"));
+	printk(KERN_ERR "%s 5\n", __func__);
+	atomic_set(&my_work.data, queue);
+	schedule_work(&my_work);
 	return 0;
 
 err_some_pages:
+	printk(KERN_ERR "%s err1\n", __func__);
 	kfree(queue->pages);
 	for(i = 0; i < queue->nr_pages; i++) {
-		ClearPageUptodate(queue->pages[i]);
 		put_page(queue->pages[i]);
 	}
 err:
+	printk(KERN_ERR "%s err2\n", __func__);
 	return -1;
 }
 #ifdef DEBUG_WITH_WRITE
@@ -135,13 +186,13 @@ static const struct file_operations log_queue_fops = {
 	.owner		= THIS_MODULE,
 	.open		= log_queue_open,
 	.release	= log_queue_release,
+	.mmap		= log_queue_mmap,
 	.unlocked_ioctl	= log_queue_ioctl,
 	.poll		= log_queue_poll,
-	.mmap		= log_queue_mmap,
 #ifdef DEBUG_WITH_WRITE
 	.write		= log_queue_write,
-#endif
 	.llseek		= log_queue_llseek,
+#endif
 };
 
 static struct miscdevice log_queue_dev = {
@@ -154,7 +205,7 @@ static struct miscdevice log_queue_dev = {
 static int log_queue_init(void)
 {
 	misc_register(&log_queue_dev);
-return 0;
+	return 0;
 }
 
 static void log_queue_exit(void)
--- kernel/zero-copy/user.c
+++ kernel/zero-copy/user.c
@@ -6,24 +6,47 @@
  ************************************************************************/
 
 #include<stdio.h>
-#include <ioctl.h>
+#include <linux/ioctl.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <mmap.h>
+#include <sys/mman.h>
 #include <string.h>
 #include <stdlib.h>
 
-#define nr_of_pages	16
-
+#define nr_of_pages	2
+#define SET_PAGE    _IOW('t', 1, int)
+#define GET_PAGE    _IOR('t', 2, int)
+#define NUM	(100000000UL)
+#define OFFSET	16
 void main() {
 	int fd = 0;
 	char *buf = NULL;
+	int page_size, mapping_size;
+	int *addr = NULL;
+	int val = 0, oldval = 0;
+	long long i = 0;
 
 	fd = open("/dev/log_queue", O_RDWR);
-	ioctl(fd, IOC_WATCH_QUEUE_NR_PAGES, nr_of_pages);
+	dprintf(2, "open :%d\n", fd);
+	ioctl(fd, SET_PAGE, nr_of_pages);
+	dprintf(2, "ioctl :%d\n", fd);
 
 	page_size = sysconf(_SC_PAGESIZE);
 	mapping_size = nr_of_pages * page_size;
 	buf = mmap(NULL, mapping_size, PROT_READ|PROT_WRITE,
 			MAP_SHARED, fd, 0);
+	printf("mmap :%d\n", fd);
+
+	printf("hello %s\n", buf);
+	addr = (long long*)(buf + OFFSET);
+	val = addr[0];
+	printf("begin:%ld\n", val);
+	for (int i = 0; i < NUM; i++) {
+		uatomic_add_return(addr, 1);
+	}
+	val = addr[0];
+	printf("begin:%ld\n", val);
+	sleep(1);
+	munmap(buf, mapping_size);
+	close(fd);
 }
